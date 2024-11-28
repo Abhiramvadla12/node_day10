fc168cc4279a04f8171309d9883885aa4436f63b
Abhiramvadla12 <abhiramvadla61@gmail.com>
2024-11-28 18:51:35 +0530

    version 1

---
+++ index.js
@@ -0,0 +1,23 @@
+let http = require("http");
+let fs = require("fs");
+let path = require("path");
+let server = http.createServer((req,res)=>{
+    //getting all files present in the folder:-
+    let files = fs.readdirSync("task");
+    // console.log(files);
+    //iterating the files names:-
+    for(i of files){
+        if(i.includes(".")){
+            // console.log(path.join(__dirname,"task", i))
+            fs.unlinkSync(path.join(__dirname,"task", i));
+        }
+        else{
+            fs.rmSync(path.join(__dirname,"task", i),{recursive:true});
+        }
+    }
+    res.end("ended successfully!!!!");
+})
+let port = 3002;
+server.listen(port,()=>{
+    console.log("server started");
+})
---
+++ index1.js
@@ -0,0 +1,39 @@
+let http = require("http");
+let fs = require("fs");
+let path = require("path");
+
+let server = http.createServer((req, res) => {
+    let directory = path.join(__dirname, "task"); // Corrected path to the "task" directory
+
+    try {
+        // Check if the "task" directory exists
+        if (fs.existsSync(directory)) {
+            // Get all files and folders in the directory
+            let files = fs.readdirSync(directory);
+
+            // Iterate over the files and delete them
+            for (let file of files) {
+                let filePath = path.join(directory, file);
+
+                if (fs.lstatSync(filePath).isDirectory()) {
+                    // Delete folder recursively
+                    fs.rmSync(filePath, { recursive: true, force: true });
+                } else {
+                    // Delete file
+                    fs.unlinkSync(filePath);
+                }
+            }
+        } else {
+            console.log("Directory 'task' does not exist.");
+        }
+    } catch (error) {
+        console.error("Error occurred while deleting files:", error);
+    }
+
+    res.end("Cleanup completed successfully!");
+});
+
+let port = 3003;
+server.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
